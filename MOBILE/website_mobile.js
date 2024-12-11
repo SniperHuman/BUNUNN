75e60a4b0d7ab08ce7a3922941b223781c4bdbf6
SniperHuman <Sniper.Human0713@gmail.com>
2024-12-12 00:46:36 +0900

    first

---
+++ MOBILE/website_mobile.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    const w = window.innerWidth;
+    const border_width = 450;
+    if (w>=border_width){
+        location.href = "website_pc.html"
+    }
+
+    
+});
